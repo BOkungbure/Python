c702eece5865ac059044eb529212998988687642
Babatunde <babatundeokungbure@gmail.com>
2025-08-17 10:17:24 -0400

    Just logging more practise

--- practise/Hello_Code.py
+++ practise/Hello_Code.py
@@ -1,4 +1,12 @@
 import Hello_World as hs
 result = hs.msg
 
-print(result.center(50))
+print(result.replace("World", "World, How are You?"))
+
+
+print("12\n34")
+
+
+print(r"this has special \\ncharcaters")
+
+print("this has special \ncharcaters")
